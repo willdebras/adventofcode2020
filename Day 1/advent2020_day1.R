da0a2ef9aac4ce263ab2cb1a301d546ee6e72f82
willdebras <wtb3na@virginia.edu>
2020-12-02 11:09:57 -0600

    annotated 4 john

--- Day 1/advent2020_day1.R
+++ Day 1/advent2020_day1.R
@@ -2,10 +2,10 @@
 
 input <- read.delim("Day 1/input_day1.txt", col.names = "input", header = F)
 
-combinations <- t(utils::combn(input$input, 2))
+combinations <- t(utils::combn(input$input, 2)) # creates two column matrix of unique combinations
 
-comb2020 <- combinations[,1] + combinations[,2]
+comb2020 <- combinations[,1] + combinations[,2] # matrix calculation to equal 2020 
 
-matrix_index <- match(2020, comb2020)
+matrix_index <- match(2020, comb2020) # match matrix index of 2020
 
-combinations[matrix_index,1] * combinations[matrix_index,2]
+combinations[matrix_index,1] * combinations[matrix_index,2] #multiplies values for final answer
