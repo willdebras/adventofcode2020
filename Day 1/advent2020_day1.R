0baffa436f23d907399ff5677b4b048c612aced7
willdebras <wtb3na@virginia.edu>
2020-12-02 12:00:40 -0600

    added day 1 part 2

--- Day 1/advent2020_day1.R
+++ Day 1/advent2020_day1.R
@@ -9,3 +9,14 @@ comb2020 <- combinations[,1] + combinations[,2] # matrix calculation to equal 20
 matrix_index <- match(2020, comb2020) # match matrix index of 2020
 
 combinations[matrix_index,1] * combinations[matrix_index,2] #multiplies values for final answer
+
+#Day 1 - part 2
+
+
+combinations <- t(utils::combn(input$input, 3)) # creates two column matrix of unique combinations
+
+comb2020 <- combinations[,1] + combinations[,2] + combinations[,3] # matrix calculation to equal 2020 
+
+matrix_index <- match(2020, comb2020) # match matrix index of 2020
+
+combinations[matrix_index,1] * combinations[matrix_index,2] * combinations[matrix_index, 3] #multiplies values for final answer
