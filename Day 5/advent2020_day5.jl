85aa5e4ac53c1106f5f9967f61da91c84c61256f
willdebras <wbonnell123@gmail.com>
2020-12-07 14:42:36 -0600

    added julia version for funsies

---
+++ Day 5/advent2020_day5.jl
@@ -0,0 +1,26 @@
+using(DelimitedFiles)
+
+input = DelimitedFiles.readdlm("Day 5/input_day5.txt")
+
+# part 1
+
+function passport(x)
+
+parsed_binary = reduce(replace, ["B"=>1, "F"=>0, "L"=>0, "R"=>1], init=x)
+row = parse(Int, parsed_binary[1:7], base = 2)
+column = parse(Int, parsed_binary[8:10], base = 2)
+seatid = row * 8 + column
+return seatid
+
+end
+
+
+seatids = passport.(input) #using . to vectorize bc julia is op
+
+maximum(seatids)
+
+# part 2
+
+seat_range = minimum(seatids):maximum(seatids)
+
+seat_diff = setdiff(seat_range, seatids)
